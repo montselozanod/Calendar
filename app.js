3d888b47aebf24a17d2d4da75873e415afc5bf4b
montselozanod <montse_967@hotmail.com>
2015-06-27 19:50:09 -0700

    integration with parse

--- app.js
+++ app.js
@@ -17,7 +17,18 @@ document.getElementById('yearCaption').innerHTML=date.getFullYear();
 document.getElementById('monthCaption').innerHTML=month[date.getMonth()];
 document.getElementById('dayCaption').innerHTML=date.getDate();
 
+  console.log("hello");
+  var quotes = 24;
+  Parse.initialize("CgPYspW53sotqoc1JeQfSS0PKyHq8wSIv46ODRN5", "6xQ056pcsVfjD1977ixHWqewmM9RJM5V8c8dnwoP");
+  var Quote = Parse.Object.extend("Quote");
+  var query = new Parse.Query(Quote);
+  query.skip(Math.floor(Math.random()*quotes));
+  query.limit(1);
+  query.find({
+    success: function(result){
+      console.log(result[0].attributes.Text);
+    },
+    error: function(error){
 
-parseResponse= function(data){
-  console.log(data.data.contents.quote);
-}
+    }
+  });
